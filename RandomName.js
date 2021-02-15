fb51834ba6d1520605d27afcbca73fd59b5255b1
Jaivin <43515464+jmzac@users.noreply.github.com>
2021-02-15 12:46:15 +0000

    Create RandomName.js
    Select a Random name from a list of names and return output

---
+++ RandomName.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+    
+    
+   var nOfPeople = names.length;
+   var pSelected = Math.floor(Math.random() * nOfPeople);
+     
+    return names[pSelected]+" is going to buy lunch today!";
+    
+    
+    
+}
